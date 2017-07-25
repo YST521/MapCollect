f82adb20ef610d3fd7ac9b00ff6f5fe570ae5c53
youxin <youxin@youxins-Mac-mini.local>
2017-07-25 17:06:36 +0800

    Map first commit

--- AliMapKit（高德地图SDK）/AliMapKit/Classes/CodeViewController.m
+++ AliMapKit（高德地图SDK）/AliMapKit/Classes/CodeViewController.m
@@ -38,6 +38,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor =[UIColor whiteColor];
     
     self.title = @"编码反编码";
     self.view.backgroundColor =[UIColor greenColor];
